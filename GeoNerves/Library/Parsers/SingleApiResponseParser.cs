44871fbc1566032ae63bc519d8e45cac63f6f4d4
Sam Ennis <sam.e.ennis@gmail.com>
2019-12-09 23:23:19 -0500

    Building out naive parsing of single API address response

--- GeoNerves/Library/Parsers/SingleApiResponseParser.cs
+++ GeoNerves/Library/Parsers/SingleApiResponseParser.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text.RegularExpressions;
 using GeoNerves.Models;
 using Newtonsoft.Json.Linq;
 
@@ -5,14 +7,32 @@ namespace GeoNerves
 {
   public static class SingleApiResponseParser
   {
-    public static Address ParseAddressFromApiResponse(string addressApiResponse)
+    public static Address ParseAddressFromApiResponse(string responseJson)
     {
-      var addressDynamic = JObject.Parse(addressApiResponse);
-      
+      var responseObject = JObject.Parse(responseJson);
+      var matches        = responseObject["result"]["addressMatches"];
+      var bestMatch      = matches[0];
+
       var address = new Address()
-      {
-        
-      };
+                    {
+                      Id = 1,
+                      Street = bestMatch["addressComponents"]["streetName"] + " " +
+                               bestMatch["addressComponents"]["suffixType"],
+                      City              = bestMatch["addressComponents"]["city"].ToString(),
+                      State             = bestMatch["addressComponents"]["state"].ToString(),
+                      Zip               = bestMatch["addressComponents"]["zip"].ToString(),
+                      Latitude          = Convert.ToDouble(bestMatch["coordinates"]["y"]),
+                      Longitude         = Convert.ToDouble(bestMatch["coordinates"]["x"]),
+                      Match             = true,
+                      MatchType         = null,
+                      NormalizedAddress = bestMatch["matchedAddress"].ToString(),
+                      TigerLineId       = null,
+                      TigerLineSide     = null,
+                      StateId           = 0,
+                      CountyId          = 0,
+                      CensusTractId     = 0,
+                      BlockId           = 0
+                    };
       return address;
     }
   }
